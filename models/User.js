d05862cbba1444bc6f08d85e77f209a04d5a2377
KJ Jackson <kjjackson619@yahoo.com>
2022-03-15 17:48:02 -0400

    user

---
+++ models/User.js
@@ -0,0 +1,44 @@
+const { Schema, model } = require('mongoose');
+
+const UserSchema = new Schema({
+    username: {
+        type: String,
+        unique: true,
+        required: true,
+        Trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    thoughts: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Thoughts'
+        }
+    ],
+
+    friends: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'User'
+        }
+    ],
+},
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        },
+        id: false
+    }
+);
+
+UserSchema.virtual('friendCount').get(function () {
+    return this.friends.reduce((total, friends) => total + friends.length + 1, 0);
+});
+
+const User = model('User', UserSchema);
+
+module.exports = User;
